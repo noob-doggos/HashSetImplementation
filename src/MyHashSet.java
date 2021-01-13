4a3e1d4783be4493763247c264a3d71e3f4b0065
Doki <duvictor514@gmail.com>
2021-01-13 07:58:58 -0800

    Cleaned up code

--- src/MyHashSet.java
+++ src/MyHashSet.java
@@ -29,7 +29,8 @@ public class MyHashSet
             HashEntry cur = elementData[hashcode];
             while (cur != null)
             {
-                if (cur.data == value) return;
+                if (cur.data == value)
+                    return;
                 if (cur.next == null)
                 {
                     cur.next = new HashEntry(value);
@@ -39,7 +40,7 @@ public class MyHashSet
                 cur = cur.next;
             }
         }
-        
+
         if (loadFactor() >= MAX_LOAD_FACTOR)
         {
             rehash();
@@ -82,33 +83,37 @@ public class MyHashSet
     // If the set does not contain the value, has no effect.
     // by: Clyde
     public void remove(int value)
-    {        
+    {
         int hashcode = hashFunction(value);
-        HashEntry cur = elementData[hashcode];  
-        HashEntry prev = null;      
-        while(cur != null)
-        {            
-            if(cur.data != value)
+        HashEntry cur = elementData[hashcode];
+        HashEntry prev = null;
+        while (cur != null)
+        {
+            if (cur.data != value)
             {
-                prev = cur; 
-                cur = cur.next;                  
-            } else {
-                if(prev == null)
+                prev = cur;
+                cur = cur.next;
+            }
+            else
+            {
+                if (prev == null)
                 {
                     elementData[hashcode] = cur.next;
-                } else {                
+                }
+                else
+                {
                     prev.next = cur.next;
                 }
                 size--;
                 break;
             }
-        }        
+        }
     }
 
     // Returns the number of elements in the queue.
     // by: Clyde
     public int size()
-    {       
+    {
         return size;
     }
 
@@ -123,7 +128,8 @@ public class MyHashSet
     // by: Eric
     private double loadFactor()
     {
-        // TODO: return size instance variable / elementData.length, but convert the divisor or dividend to double.
+        // TODO: return size instance variable / elementData.length, but convert
+        // the divisor or dividend to double.
         return (double) this.size / elementData.length;
     }
 
@@ -131,35 +137,25 @@ public class MyHashSet
     // by: Eric
     private void rehash()
     {
-        // TODO:
-        // assign HashEntry[] oldData to elementData.
-        // reinitialize elementData to a new HashArray with size 2 * oldData.length.
-        // set size to 0.
-        // then, for each HashEntry in oldData:
-        //  if the HashEntry is not null,
-        //      assign HashEntry cur to the current HashEntry.
-        //          while cur is not NULL:
-        //              call add(cur.data).
-        //              assign cur to cur.next.
         HashEntry[] oldData = elementData;
 
-            // formation of new table
-            elementData = new HashEntry[2 * oldData.length];
-            size = 0;
-        
-            // copying the table
-            for (HashEntry curEntry : oldData)
+        // formation of new table
+        elementData = new HashEntry[2 * oldData.length];
+        size = 0;
+
+        // copying the table
+        for (HashEntry curEntry : oldData)
+        {
+            HashEntry cur = curEntry;
+            while (cur != null)
             {
-                HashEntry cur = curEntry;
-                while (cur != null)
-                {
-                    add(cur.data);
-                    cur = cur.next;
-             }
-         }
-        
+                add(cur.data);
+                cur = cur.next;
+            }
+        }
+
     }
-    
+
     // Returns a string representation of this queue, such as "[10, 20, 30]";
     // The elements are not guaranteed to be listed in sorted order.
     public String toString()
